5961f0b1c927d6c578a457adc335e7d64620cf88
Jchu4 <jerome.chuame@gmail.com>
2021-06-06 22:23:16 +0800

    Create main directories and essential files

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+
+    'brace-style': 'off',
+    'import/extensions': ['error', 'ignorePackages'],
+    'linebreak-style': 'off',
+    'no-console': 'off',
+    'no-param-reassign': ['error', { props: false }],
+  },
+};
