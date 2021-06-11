9758c520354cd56c3be1a45425d1b9794c479c63
Jchu4 <jerome.chuame@gmail.com>
2021-06-11 18:34:20 +0800

    Remove googlesheets related code and libraries

--- seeders/20210606141901-seed-data.js
+++ seeders/20210606141901-seed-data.js
@@ -1,33 +1,34 @@
-import { GoogleSpreadsheet } from "google-spreadsheet";
-import creds from "../client_secret.js";
+import jsSHA from 'jssha';
+import parse from 'csv-parse';
+import fs from 'fs';
 
-const SHEET_ID = "15CWgsB3aTdEcY_dR7pnCOR70SxQP7LljEwGTxfaQOZo";
-const doc = new GoogleSpreadsheet(SHEET_ID);
-
-async function accessSheets() {
-  await doc.useServiceAccountAuth({
-    client_email: creds.client_email,
-    private_key: creds.private_key,
-  });
-
-  await doc.loadInfo(); // loads document properties and worksheets
-  console.log("Doc", doc.title);
-
-  const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
-  console.log(`Title: ${sheet.title}, Rows: ${sheet.rowCount}`);
-}
-
-accessSheets();
-
-
-//https://www.youtube.com/watch?v=ftxroBc7mi4
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8' });
+    shaObj.update('pw123');
+    const hashedPassword = shaObj.getHash('HEX');
+    const usersList = [
+      {
+        name: 'jerome',
+        email: 'jerome123@gmail.com',
+        password: hashedPassword,
+        created_at: new Date(),
+        updated_at: new Date(),
+      }, 
+      {
+        name: 'chua',
+        email: 'chua123@gmail.com',
+        password: hashedPassword,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ];
+    await queryInterface.bulkInsert('users', usersList);
   },
 
   down: async (queryInterface) => {
-    await queryInterface.bulkDelete('expense', null, {});
-    await queryInterface.bulkDelete('user', null, {});
+    // await queryInterface.bulkDelete('expenses', null, {});
+    await queryInterface.bulkDelete('users', null, {});
 
   }
 };
