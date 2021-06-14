5045c5798a3328016cabd5194705e2c9f77fffe2
Jchu4 <jerome.chuame@gmail.com>
2021-06-14 10:08:59 +0800

    Update controller. Create first route to get all transactions (hardcode userId)

--- controllers/transactions.mjs
+++ controllers/transactions.mjs
@@ -1,3 +1,24 @@
 export default function initTransactionsController(db) {
+  
+  const getTransactions = async (req, res) => {
+    // const { userId } = req.params;
+    let userId = 1;
 
+    try {
+      const transactions = await db.Transaction.findAll({
+        where: {
+          userId: Number(userId),
+        },
+      });
+
+      res.send(transactions);
+    } catch (err) {
+      console.log("Error found while getTransactions:", err);
+    }
+  }
+
+
+  return {
+    getTransactions,
+  }
 }
--- routes.mjs
+++ routes.mjs
@@ -4,5 +4,9 @@ import initTransactionsController from './controllers/transactions.mjs';
 import initUserControllers from './controllers/users.mjs';
 
 export default function bindRoutes(app) {
+  const TransactController = initTransactionsController(db);
+  app.get('/gettransactions/:userId', TransactController.getTransactions);
+
+  const UsersController = initUserControllers(db);
 
 }
