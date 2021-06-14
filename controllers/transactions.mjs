b05aeb256a36bbb681fa432494bd370bbba22dac
Jchu4 <jerome.chuame@gmail.com>
2021-06-14 11:41:43 +0800

    Create addTransaction route (to be tested)

--- controllers/transactions.mjs
+++ controllers/transactions.mjs
@@ -11,14 +11,34 @@ export default function initTransactionsController(db) {
         },
       });
 
+      console.log("TRANSACTIONS ====================\n\n", transactions);
+
       res.send(transactions);
     } catch (err) {
       console.log("Error found while getTransactions:", err);
     }
   }
 
+  const addTransaction = async (req, res) => {
+    // const { userId } = req.cookies;
+    let userId = 1;
+
+    const {transactionData} = req.body;
+
+    try {
+      const user = await db.User.findByPk(Number(userId));
+      const add = await user.addTransaction(transactionData);
+
+      console.log("Add succesfully: -----", add);
+
+      res.send("SUCCESS")
+    } catch (err) {
+       console.log("Error found while addTransaction:", err);
+    }
+  }
 
   return {
     getTransactions,
+    addTransaction,
   }
 }
--- routes.mjs
+++ routes.mjs
@@ -6,7 +6,8 @@ import initUserControllers from './controllers/users.mjs';
 export default function bindRoutes(app) {
   const TransactController = initTransactionsController(db);
   app.get('/gettransactions/:userId', TransactController.getTransactions);
-
+  app.post('/addtransaction/:userId', TransactController.addTransaction);
+  
   const UsersController = initUserControllers(db);
 
 }
