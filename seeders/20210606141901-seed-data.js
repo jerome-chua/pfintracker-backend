6a07132215de6d259004f9c574159b8b7a0c43f3
Jchu4 <jerome.chuame@gmail.com>
2021-06-21 16:32:09 +0800

    Reformat code according eslint, and change seeder file in gitignore

--- seeders/20210606141901-seed-data.js
+++ seeders/20210606141901-seed-data.js
@@ -13,7 +13,7 @@ module.exports = {
         password: hashedPassword,
         created_at: new Date(),
         updated_at: new Date(),
-      }, 
+      },
       {
         name: 'chua',
         email: 'chua123@gmail.com',
@@ -24,36 +24,18 @@ module.exports = {
     ];
     await queryInterface.bulkInsert('users', usersList);
 
-    // const expenseColors = ['#b50d12', '#bf2f1f', '#c9452c', '#d3583a', '#dc6a48', '#e57c58', '#ee8d68', '#f79d79', '#ffae8a', '#f55b5f', '#0bc77e', '#04e38d', '#00ff9d']; 
-
-    // const expenseCategories = [
-    //   { type: 'Bills', color: expenseColors[0] },
-    //   { type: 'Books', color: expenseColors[1] },
-    //   { type: 'Church', color: expenseColors[2] },
-    //   { type: 'Transportation', color: expenseColors[3] },
-    //   { type: 'Food & Drinks', color: expenseColors[4] },
-    //   { type: 'Shopping', color: expenseColors[5] },
-    //   { type: 'Gifts', color: expenseColors[6] },
-    //   { type: 'Entertainment', color: expenseColors[7] },
-    //   { type: 'Haircuts', color: expenseColors[8] },
-    //   { type: 'Investment', color: expenseColors[9] },
-    //   { type: 'Medical', color: expenseColors[10] },
-    //   { type: 'Sports', color: expenseColors[11] },
-    //   { type: 'Salary', color: expenseColors[12] },
-    // ];
     const transactionsList = [];
-    
-    for (let i=1; i<csvData.length; i++) {
-      
-      let row = csvData[i];
+
+    for (let i = 1; i < csvData.length; i++) {
+      const row = csvData[i];
       const [transactDate, wallet, transactionType, categoryName, amount, note, hashTag] = row;
-  
-      let transaction = {
-        user_id: 1, 
+
+      const transaction = {
+        user_id: 1,
         transaction_type: transactionType,
         amount: parseFloat(amount.substring(1)),
         currency: 'SGD',
-        note: note,
+        note,
         category: categoryName,
         hashtag: hashTag,
         created_at: new Date(Date.parse(transactDate)),
@@ -68,5 +50,5 @@ module.exports = {
   down: async (queryInterface) => {
     await queryInterface.bulkDelete('transactions', null, {});
     await queryInterface.bulkDelete('users', null, {});
-  }
+  },
 };
--- seeders/csv-data.js
+++ seeders/csv-data.js
@@ -1,19 +1,18 @@
 const parse = require('csv-parse');
 const fs = require('fs');
 
-let csvData = [];
-  fs.createReadStream(`${__dirname}/seed_data.csv`)
+const csvData = [];
+fs.createReadStream(`${__dirname}/seed_data.csv`)
   .pipe(
-      parse({
-        delimiter: ','
-    })
+    parse({
+      delimiter: ',',
+    }),
   )
   .on('data', (dataRow) => {
-    csvData.push(dataRow)
+    csvData.push(dataRow);
   })
   .on('end', () => {
     console.log(csvData);
-  })
+  });
 
 module.exports = csvData;
- 
